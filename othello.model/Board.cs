62dd3d5a10842b56e471f53ccd7ac11409765df4
nathanLeSommer <lesommer.nathan@gmail.com>
2018-02-27 15:47:26 +0100

    Changing model a lil' bit

--- othello.model/Board.cs
+++ othello.model/Board.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using othello.ia;
 
 namespace othello.model
 {
     public class Board
     {
-        //public Disc[,] Discs { get; set; }
         public int[,] Tiles { get; set; }    
         public Board()
         {
             Tiles = new int[8,8];
-            //Discs = new Disc[8,8];
+            for(int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    Tiles[i, j] = 0;
+                }
+            }
+        }
+
+        public KeyValuePair<int,int> PlayMoveIA()
+        {
+            KeyValuePair<int,int> pos = IANoob.SelectPosition(Tiles);
+            Tiles[pos.Key, pos.Value] = 2;
+            return pos;
         }
     }
 }
